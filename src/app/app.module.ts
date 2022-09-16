d1f31a907bb8bbd9d06d7b98b7df05bdbeb284d6
egWarriors <egwarriors080@gmail.com>
2022-09-16 17:41:34 +0000

    continue set the main part of the index page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './pages/index/index.component';
 import { HeaderComponent } from './pages/index/components/header/header.component';
 import { MainIndexComponent } from './pages/index/components/main-index/main-index.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, IndexComponent, HeaderComponent, MainIndexComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    HeaderComponent,
+    MainIndexComponent,
+  ],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/index/components/main-index/main-index.component.ts
+++ src/app/pages/index/components/main-index/main-index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-index',
@@ -9,7 +10,8 @@ export class MainIndexComponent implements OnInit {
   prefixe_img_path = '../../../../../assets/images/';
   img1_right_path = this.prefixe_img_path + 'services_image1.jpg';
   img_connector_path = this.prefixe_img_path + 'connecteur_img.png';
-  img2_left_path = this.prefixe_img_path + 'paiementplusieursfois.jpg';
+  img2_left_path = this.prefixe_img_path + 'paiementplusieursfos.png';
+  faPlus = faPlus;
 
   constructor() {}
 
